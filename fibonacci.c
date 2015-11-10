164f3a402ba9412335e78e106c884a359a5d1efb
justingapper <gappe102@mail.chapman.edu>
2015-11-10 10:25:51 -0800

    add fibonacci long

--- fibonacci.c
+++ fibonacci.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-
-main() {
-	int a = 0;
-	int b = 1;
-	int x = 0;
-	while(b < 101) {
+/*fibonacci sequence*/
+int main(void) {
+	long a = 1L; /*first value*/
+	long b = 1L; /*second value*/
+	long x = 1L; /*result value (iteration)*/
+	int c = 0; /*counter*/
+	while(c < 101) {
 		printf("%d\n", a);
 		x = b;
-		b = a+b;
+		b = a + b;
 		a = x;
+		c++;
 	}
 	return 0;
 }
+
+/*negative numbers show up because it exceeds the maximum number that can be held using int ((2^31)-1 = 2147483647).  This can be corrected by declaring the variable type long. */
