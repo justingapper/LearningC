c8706a207a13b13bea50ba8730ac98a47869074c
justingapper <gappe102@mail.chapman.edu>
2015-11-17 20:56:23 -0800

    Update for long double

--- complex.c
+++ complex.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include "complex.h"
 
 typedef struct complex{
-	float x;
-	float y;
+	long double x;
+	long double y;
 }complex;
 
 complex mult2(complex a, complex b);
@@ -20,24 +21,24 @@ int main(){
 int test(){
 	complex a, b, z, c, out_mult2, out_square, out_add2, out_juliamap, out_complex_print;
 	printf("\nEnter real and imaginary parts of a: ");
-	scanf("%f%f", &a.x,&a.y);
+	scanf("%Lf%Lf", &a.x,&a.y);
 	printf("\nEnter real and imaginary parts of b: ");
-	scanf("%f%f", &b.x,&b.y);
+	scanf("%Lf%Lf", &b.x,&b.y);
 	out_mult2 = mult2(a,b);
 	out_square = square(a);
 	out_add2 = add2(a,b);
-	printf("\nmult2 (a+b): %f + %fi",out_mult2.x, out_mult2.y);
-	printf("\nsquare (a): %f + %fi",out_square.x, out_square.y);
-	printf("\nadd2 (a+b): %f + %fi",out_add2.x, out_add2.y);
+	printf("\nmult2 (a+b): %Lf + %Lfi",out_mult2.x, out_mult2.y);
+	printf("\nsquare (a): %Lf + %Lfi",out_square.x, out_square.y);
+	printf("\nadd2 (a+b): %Lf + %Lfi",out_add2.x, out_add2.y);
 
 	printf("\nEnter real and imaginary parts of z: ");
-	scanf("%f%f", &z.x,&z.y);
+	scanf("%Lf%Lf", &z.x,&z.y);
 	printf("\nEnter real and imaginary parts of c: ");
-	scanf("%f%f", &c.x,&c.y);
+	scanf("%Lf%Lf", &c.x,&c.y);
 	out_juliamap = juliamap(z,c);
 	out_complex_print = complex_print(z);
-	printf("\njuliamap (z^2 + c): %f + %fi",out_juliamap.x, out_juliamap.y);
-	printf("\ncomplex_print: z = %f + %fi",out_complex_print.x, out_complex_print.y);
+	printf("\njuliamap (z^2 + c): %Lf + %Lfi",out_juliamap.x, out_juliamap.y);
+	printf("\ncomplex_print: z = %Lf + %Lfi",out_complex_print.x, out_complex_print.y);
 	return 0;
 }
 
