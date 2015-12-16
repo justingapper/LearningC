e989686eb62124be94d1d67f682b594eaad730c8
justingapper <justin.gapper@gmail.com>
2015-12-16 03:17:05 +0000

    Add complex.c and complex.h (HW8)

--- complex.c
+++ complex.c
@@ -1,37 +1,66 @@
 #include <stdio.h>
 #include "complex.h"
 
-complex mult2(complex a, complex b){
+// Set a complex number
+//void set(complex *c, tyde a, tyde b){
+//  c->x=a;
+//  c->y=b;
+//}
+
+// Multiply two complex numbers
+
+complex mult2(complex *a, complex *b){
 	complex out_mult2;
-	out_mult2.x = (a.x*b.x)-(a.y*b.y);
-	out_mult2.y = (a.x*b.y)+(b.x*a.y);
+	out_mult2.x = (a->x*b->x)-(a->y*b->y);
+	out_mult2.y = (a->x*b->y)+(b->x*a->y);
 	return(out_mult2);
 }
 
-complex square(complex a){
-	complex out_square;
-	out_square.x = ((a.x*a.x) - (a.y*a.y));
-	out_square.y = (2*a.x*a.y);
-	return(out_square);
-}	
+// Square a complex number
+//complex square(complex *a){
+//  return mult2(a,a);
+//}
 
-complex add2(complex a, complex b){
+complex square(complex *a){
+	complex out_square;
+	out_square.x = ((a->x*a->x) - (a->y*a->y));
+	out_square.y = (2*a->x*a->y);
+	return(out_square);
+}
+
+// Add two complex numbers
+//complex add2(complex *a, complex *b){
+//  complex z;
+//  z.x=(a->x)+(b->x);
+//  z.y=(a->y)+(b->y);
+//  return z;
+//}
+
+complex add2(complex *a, complex *b){
 	complex out_add2;
-	out_add2.x = a.x + b.x;
-	out_add2.y = a.y + b.y;
+	out_add2.x = a->x + b->x;
+	out_add2.y = a->y + b->y;
 	return(out_add2);
 }
 
-complex juliamap(complex z, complex c){
+// Apply juliamap to two complex numbers
+//complex juliamap(complex *a, complex *b){
+//  complex c = square(a);
+//  return add2(&c,b);
+//}
+
+complex juliamap(complex *a, complex *b){
 	complex out_juliamap;
-	out_juliamap = add2(square(z),c);
-	return(out_juliamap);
+	out_juliamap = square(a);
+	return add2(&out_juliamap,b);
 }
 
-complex complex_print(complex z){
-	complex out_complex_print;
-	out_complex_print = z;
-	return(out_complex_print);
+// Print a complex number
+complex complex_print(complex *a){
+  if ((a->y)<0){
+    printf("z=%Lf%Lfi\n",a->x,a->y);
+  }
+  else{
+    printf("z=%Lf+%Lfi\n",a->x,a->y);
+  }
 }
-
-
--- complex.h
+++ complex.h
@@ -1,12 +1,11 @@
+typedef long double tyde;
+typedef struct complex {
+  long double x;
+  long double y;
+} complex;
 
-typedef struct complex{
-	long double x;
-	long double y;
-}complex;
-
-complex mult2(complex a, complex b);
-complex square(complex a);
-complex add2(complex a, complex b);
-complex juliamap(complex z, complex c);
-complex complex_print(complex z);
-
+complex mult2(complex *a, complex *b);
+complex square(complex *a);
+complex add2(complex *a,complex *b);
+complex juliamap(complex *a, complex *b);
+complex complex_print(complex *a);
